c5a38b17a6c284676e6528c812e6aa0f3720a220
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-15 04:31:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
   arr = []
+  if array.length == 1
+    return array
+  elsif array.length == 2
+    array.join(" and")
   for int in (0..array.length-2) do
     arr << array[int]
   end
