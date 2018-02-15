3a1ea125442ec458b9d325af5a19327334efaef3
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-15 04:30:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   for int in (0..array.length-2) do
     arr << array[int]
   end
-  str = arr.join(" , ")
+  str = arr.join(", ")
   str << ", and " + array[-1]
   return str
 end
