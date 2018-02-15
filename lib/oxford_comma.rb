3baea64f4b81e2cbfb6468b15deeba6f8a96316d
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-15 04:32:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,13 @@ def oxford_comma(array)
   if array.length == 1
     return array
   elsif array.length == 2
-    array.join(" and")
-  for int in (0..array.length-2) do
-    arr << array[int]
+    array.join(" and ")
+  else
+    for int in (0..array.length-2) do
+      arr << array[int]
+    end
+    str = arr.join(", ")
+    str << ", and " + array[-1]
+    return str
   end
-  str = arr.join(", ")
-  str << ", and " + array[-1]
-  return str
 end
